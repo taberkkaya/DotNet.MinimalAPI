620f368670ac16ba137b0d213d6f4f2f74e4d912
taberkkaya <taberkkaya@gmail.com>
2025-02-13 04:37:08 +0300

    Introduction done

---
+++ 2.GettingStarted/Minimal.API/Example.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimal.API
+{
+    public sealed class Example
+    {
+        public static string SomeMethod()
+        {
+            return "This is coming from a some methods";
+        }
+    }
+}
--- 2.GettingStarted/Minimal.API/Program.cs
+++ 2.GettingStarted/Minimal.API/Program.cs
@@ -1,3 +1,5 @@
+using Minimal.API;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -13,6 +15,36 @@ app.MapGet("slow-request", async () => {
     await Task.Delay(2000);
 
     return Results.Ok(new { Message = "Slow API is working..."});
+});
+
+app.MapGet("get", () => "This is a GET");
+app.MapPost("post", () => "This is a POST");
+app.MapPut("put", () => "This is a PUT");
+app.MapDelete("delete", () => "This is a DELETE");
+
+app.MapMethods("options-or-head", 
+    new[] {"HEAD","OPTIONS"},
+    () => "Hello from either options or head");
+
+var handler = () => "This is coming from a var";
+
+app.MapGet("handler", handler);
+
+app.MapGet("from-class", Example.SomeMethod);
+
+app.MapGet("get-params/{age:int}", (int age) =>
+{
+    return $"Age provided was {age}";
+});
+
+app.MapGet("cars/{carId:regex(^[a-z0-9+$])}", (string carId) =>
+{
+    return $"Card ID provided was : {carId}";
+});
+
+app.MapGet("books/{isbn:length(11)}", (string isbn) =>
+{
+    return $"Isbn provided was: {isbn}";
 });
 
 app.Run();
